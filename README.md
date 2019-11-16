98cb260f2d129f07d85acdd82d0462f38a8c3568
mattia richetto <mattia.richetto@gmail.com>
2019-11-15 23:48:48 -0800

    prettier

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ bee-blues
 ## Resources
 
 - https://dev.to/hagnerd/setting-up-tailwind-with-create-react-app-4jd
+
+- https://create-react-app.dev/docs/deployment/#github-pages
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Button from "./components/Button";
+import React from 'react';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
       <h1>Super cool page</h1>
-      <Button onClick={() => console.log("I was clicked")}>
+      <Button onClick={() => console.log('I was clicked')}>
         I am a button
       </Button>
     </div>

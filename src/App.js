7ed0953925c5c24a0a3e7622218f5ece4a5eac38
mattia richetto <mattia.richetto@gmail.com>
2019-11-17 14:10:55 -0800

    show loading status

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import { name, version } from '../package.json';
 import FillInTheBlanks from './FillInTheBlanks/FillInTheBlanks';
@@ -5,13 +7,16 @@ import makeModels from './makeModels';
 
 function App() {
   const [models, setModels] = useState([]);
+  const [waiting, setWaiting] = useState(true);
 
   useEffect(() => {
     makeModels((error, models) => {
       if (error) {
         console.log('makeModels error:', error);
+        setWaiting(false);
       } else {
         setModels(models);
+        setWaiting(false);
       }
     });
   }, []);
@@ -21,13 +26,21 @@ function App() {
       <h1 className="capitalize font-normal hover:font-bold text-4xl text-gray-900">
         {name}
       </h1>
-      <ul>
-        {models.map((model, index) => (
-          <li key={index}>
-            <FillInTheBlanks id={index} model={model} />
-          </li>
-        ))}
-      </ul>
+      {waiting ? (
+        <FontAwesomeIcon
+          className="text-4xl"
+          icon={faCircleNotch}
+          spin={true}
+        />
+      ) : (
+        <ul>
+          {models.map((model, index) => (
+            <li key={index}>
+              <FillInTheBlanks id={index} model={model} />
+            </li>
+          ))}
+        </ul>
+      )}
       <small className="font-normal hover:font-bold text-gray-700 text-sm">
         v{version}
       </small>

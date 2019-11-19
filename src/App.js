baa9813faa751e42841364d732cd631b11ed4575
John Cutler <johnpcutler@gmail.com>
2019-11-18 20:55:31 -0800

    set max width of content

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   }, []);
 
   return (
-    <div className="flex flex-col font-serif items-center mx-auto my-12 w-3/4">
+    <div className="flex flex-col font-serif items-center mx-auto my-12 max-w-3xl">
       <h1 className="capitalize font-normal hover:font-bold text-4xl text-gray-900">
         {name}
       </h1>

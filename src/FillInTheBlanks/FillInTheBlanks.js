365fbd8ca20acdf43f74c7b34f5cc20806cecc3b
John Cutler <johnpcutler@gmail.com>
2019-11-19 04:19:15 -0800

    experiment with highlights

--- src/FillInTheBlanks/FillInTheBlanks.js
+++ src/FillInTheBlanks/FillInTheBlanks.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Content from './Content';
-import Radio from './Radio';
+import Checkbox from './Checkbox';
 import modes from './modes';
 
 function FillInTheBlanks({ id, model }) {
@@ -10,44 +10,19 @@ function FillInTheBlanks({ id, model }) {
     <>
       <div className="leading-loose text-lg">
         <Content mode={mode} model={model} />
+				<div className="inline-block pl-3">
+		    {model.examples && (
+		        <Checkbox
+		          id={id}
+		          mode={mode}
+		          onChange={() => setMode((mode === "examples") ? modes.inputs : modes.examples)}
+		          value={modes.examples}
+		        />
+		    )}
+				</div>
       </div>
       <ul className="p-4">
-        <li>
-          <Radio
-            id={id}
-            mode={mode}
-            onChange={() => setMode(modes.blanks)}
-            value={modes.blanks}
-          />
-        </li>
-        {model.examples && (
-          <li>
-            <Radio
-              id={id}
-              mode={mode}
-              onChange={() => setMode(modes.examples)}
-              value={modes.examples}
-            />
-          </li>
-        )}
-        <li>
-          <Radio
-            id={id}
-            mode={mode}
-            onChange={() => setMode(modes.input)}
-            value={modes.input}
-          />
-        </li>
-        {model.prompts && (
-          <li>
-            <Radio
-              id={id}
-              mode={mode}
-              onChange={() => setMode(modes.prompts)}
-              value={modes.prompts}
-            />
-          </li>
-        )}
+        
       </ul>
     </>
   );

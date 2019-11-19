0d58a2d565f88618d603ddd5d4ff962ea6b796b3
mattia richetto <mattia.richetto@gmail.com>
2019-11-19 09:40:30 -0800

    prettier:fix

--- src/FillInTheBlanks/Checkbox.js
+++ src/FillInTheBlanks/Checkbox.js
@@ -3,37 +3,24 @@ import React from 'react';
 function Checkbox({ id, mode, onChange, value }) {
   const inputId = `${id}-${value}`;
   return (
-    <>
-      
-		  <label 
-		    htmlFor={inputId}
-		    className="cursor-pointer"
-		  >
-		  <div className="relative">
+    <label htmlFor={inputId} className="cursor-pointer">
+      <div className="relative">
+        <input
+          checked={mode === value}
+          data-testid={inputId}
+          id={inputId}
+          name={id}
+          onChange={onChange}
+          type="checkbox"
+          value={value}
+          className="hidden"
+        />
 
-		      <input
-		        checked={mode === value}
-		        data-testid={inputId}
-		        id={inputId}
-		        name={id}
-		        onChange={onChange}
-		        type="checkbox"
-		        value={value}
-						className="hidden"
-		      />
+        <div className="toggle__line w-6 h-3 bg-gray-400 rounded-full shadow-inner" />
 
-		      <div
-		        className="toggle__line w-6 h-3 bg-gray-400 rounded-full shadow-inner"
-		      ></div>
-
-		      <div
-		        className="toggle__dot absolute w-4 h-4 bg-gray-300 rounded-full shadow inset-y-0 left-0"
-		      ></div>
-		    </div>
-				
-		  </label>
-						
-    </>
+        <div className="toggle__dot absolute w-4 h-4 bg-gray-300 rounded-full shadow inset-y-0 left-0" />
+      </div>
+    </label>
   );
 }
 
--- src/FillInTheBlanks/FillInTheBlanks.js
+++ src/FillInTheBlanks/FillInTheBlanks.js
@@ -10,20 +10,20 @@ function FillInTheBlanks({ id, model }) {
     <>
       <div className="leading-loose text-lg">
         <Content mode={mode} model={model} />
-				<div className="inline-block pl-3">
-		    {model.examples && (
-		        <Checkbox
-		          id={id}
-		          mode={mode}
-		          onChange={() => setMode((mode === "examples") ? modes.inputs : modes.examples)}
-		          value={modes.examples}
-		        />
-		    )}
-				</div>
+        <div className="inline-block pl-3">
+          {model.examples && (
+            <Checkbox
+              id={id}
+              mode={mode}
+              onChange={() =>
+                setMode(mode === 'examples' ? modes.inputs : modes.examples)
+              }
+              value={modes.examples}
+            />
+          )}
+        </div>
       </div>
-      <ul className="p-4">
-        
-      </ul>
+      <ul className="p-4"></ul>
     </>
   );
 }

bc47dd51395d8dc13f0159e953c88649aea81d49
mattia richetto <mattia.richetto@gmail.com>
2019-11-21 21:30:05 -0800

    Updates

---
+++ precache-manifest.f36a89b5cc6ab9f82d39caccbf8551e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c005d38147a6486390c315d6cc65297d",
+    "url": "/bee-blues/index.html"
+  },
+  {
+    "revision": "fe5a875e9ee811b0e5e8",
+    "url": "/bee-blues/static/css/main.b04c2ac1.chunk.css"
+  },
+  {
+    "revision": "b4a12ac6cdc6c3a3b0df",
+    "url": "/bee-blues/static/js/2.781fd821.chunk.js"
+  },
+  {
+    "revision": "fe5a875e9ee811b0e5e8",
+    "url": "/bee-blues/static/js/main.f810e2aa.chunk.js"
+  },
+  {
+    "revision": "f707956e94dd73e28f03",
+    "url": "/bee-blues/static/js/runtime-main.69b2bb3a.js"
+  }
+]);

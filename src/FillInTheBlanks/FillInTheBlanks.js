f2cbfd4abe461512154c81bf3bc2f3b4ff7d7d32
mattia richetto <mattia.richetto@gmail.com>
2019-11-21 19:03:20 -0800

    cleanup

--- src/FillInTheBlanks/FillInTheBlanks.js
+++ src/FillInTheBlanks/FillInTheBlanks.js
@@ -9,22 +9,20 @@ function FillInTheBlanks({ id, model }) {
   return (
     <div className="leading-loose text-lg my-5">
       <Content mode={mode} model={model} />
-      <div className="inline-block pl-3">
-        {model.examples && (
-          <Checkbox
-            id={id}
-            onChange={value => {
-              if (value === values.off) {
-                setMode(modes.examples);
-              }
-              if (value === values.on) {
-                setMode(modes.input);
-              }
-            }}
-            value={mode === modes.examples ? values.on : values.off}
-          />
-        )}
-      </div>
+      {model.examples && (
+        <Checkbox
+          id={id}
+          onChange={value => {
+            if (value === values.off) {
+              setMode(modes.examples);
+            }
+            if (value === values.on) {
+              setMode(modes.input);
+            }
+          }}
+          value={mode === modes.examples ? values.on : values.off}
+        />
+      )}
       <hr className="mt-3" />
     </div>
   );

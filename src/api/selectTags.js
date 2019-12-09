4b885d38fcba6372d4c13500cf81c28a20e00ce8
mattia richetto <mattia.richetto@gmail.com>
2019-12-09 06:52:31 -0800

    add REACT_APP_FLIGHT_MODE

--- src/api/selectTags.js
+++ src/api/selectTags.js
@@ -1,16 +1,23 @@
 import { ASC, NAME, TAGS } from './constants';
+import tag from '../mocks/tag';
 import makeTag from './makeTag';
 import select from './select';
 
+const { REACT_APP_FLIGHT_MODE } = process.env;
+
 function selectTags(callback) {
-  select({
-    callback,
-    map: makeTag,
-    options: {
-      sort: [{ field: NAME, direction: ASC }]
-    },
-    tableName: TAGS
-  });
+  if (REACT_APP_FLIGHT_MODE === 'on') {
+    callback(null, [tag]);
+  } else {
+    select({
+      callback,
+      map: makeTag,
+      options: {
+        sort: [{ field: NAME, direction: ASC }]
+      },
+      tableName: TAGS
+    });
+  }
 }
 
 export default selectTags;

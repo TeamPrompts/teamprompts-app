259ba594cd8dc475a7b3c4056d1d495a354ceaa0
mattia richetto <mattia.richetto@gmail.com>
2019-12-23 14:51:58 +0100

    100% code cov CategoryPage

--- src/containers/App/CategoryPage.test.js
+++ src/containers/App/CategoryPage.test.js
@@ -1,6 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import { create } from 'react-test-renderer';
 import { HashRouter as Router } from 'react-router-dom';
+import { useAmplitude } from '../../instrumentation/AmplitudeHookProvider';
 import fitbs from '../../mocks/fitbs';
 import tag from '../../mocks/tag';
 import tags from '../../mocks/tags';
@@ -19,9 +21,21 @@ describe('CategoryPage', () => {
   it('to match snapshot', () => {
     const tree = create(
       <Router>
-        <CategoryPage fitbs={fitbs} match={{ url: '/TBD' }} tags={tags} />
+        <CategoryPage fitbs={fitbs} match={{ url: `/tag.slug` }} tags={tags} />
       </Router>
     ).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('tagEmpty', () => {
+    const { logEvent } = useAmplitude();
+
+    render(
+      <Router>
+        <CategoryPage fitbs={fitbs} match={{ url: `/tag.slug` }} tags={[]} />
+      </Router>
+    );
+
+    expect(logEvent).not.toBeCalled();
   });
 });
